5b9f38b34ac7a839fd030f003414a0c4ced6ad7b
ethanchilds <ee11.childs@gmail.com>
2022-03-09 09:21:05 -0700

    finished termianl service?

---
+++ jumper_game/game_class/terminal_service.py
@@ -0,0 +1,18 @@
+class TerminalService:
+    def print_word(self, word_list):
+        for letter in word_list:
+            print(letter, end = ' ')
+
+    def display_jumper(self, parachute, jumper):
+        for piece in parachute:
+            print(piece)
+        print(jumper)
+    
+    def get_letter_guess(self):
+        letter = input('Guess a letter [a-z]: ')
+        return letter
+
+    def print_break(Self):
+        print('^^^^^^^')
+
+
