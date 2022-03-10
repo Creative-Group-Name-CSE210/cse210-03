e2eeef5a73ad9f096606f86f3c93f85877fa27f5
ethanchilds <ee11.childs@gmail.com>
2022-03-10 09:27:37 -0700

    finished loop

--- jumper_game/game_class/director.py
+++ jumper_game/game_class/director.py
@@ -1,8 +1,8 @@
 # director Class 
 # imports 
-from game_class.word import Word
-from game_class.terminal_service import TerminalService
-from game_class.jumper import Jumper
+from word import Word
+from terminal_service import TerminalService
+from jumper import Jumper
 
 class Director:
 
@@ -16,15 +16,14 @@ class Director:
 
     def game_loop(self):
         self.do_outputs()
-        while self.is_alive:
+        while self.jumper.is_alive:
             self.get_input()
             self.do_updates()
             self.do_outputs()
     
 
     def get_input(self):
-        pass
-    
+        self.guess_letter = self.terminal.get_letter_guess()    
 
     def do_updates(self):
         if self.word.check_letter(self.guess_letter) == True:
@@ -33,5 +32,12 @@ class Director:
             self.jumper.remove_line()
     
     def do_outputs(self):
-        print(Word)
-        print(Jumper)
+        self.terminal.print_word(self.word.guessed_word_list)
+        print()
+        parachute, jumper = self.jumper.return_jumper()
+        self.terminal.display_jumper(parachute, jumper)
+
+
+
+director = Director()
+director.game_loop()
